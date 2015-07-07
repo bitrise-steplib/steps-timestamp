7528eff8ce522002cab8ac82bd1a591ae488bb2d
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-07-07 17:00:32 +0200

    step.sh, step.yml fixes

--- step.sh
+++ step.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+set -e
+
 THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Start go program
 cd "${THIS_SCRIPTDIR}"
 
+set -v
+
 go run ./step.go
-ex_code=$?
-
-if [ ${ex_code} -eq 0 ] ; then
-  exit 0
-fi
-
-exit 1
